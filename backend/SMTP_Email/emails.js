13e10ec15e92685a12ea4ef7b0db270e2d4a3d8a
Lunatic5578 <zeedanmohsin36@gmail.com>
2025-01-29 16:35:29 +0530

    Mern-Auth

---
+++ backend/SMTP_Email/emails.js
@@ -0,0 +1,95 @@
+import dotenv from "dotenv";
+import nodemailer from "nodemailer"
+
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
+
+dotenv.config();
+
+// Configure the transporter with SMTP settings
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587, // or 587, 465
+  auth: {
+    user: process.env.SENDER_MAIL, 
+    pass: process.env.GMAIL_SMTP_PASS, 
+  },
+});
+
+// General-purpose function to send emails
+const sendEmail = async (toEmail, subject, htmlContent) => {
+  
+    const mailOptions = {
+      from: process.env.SENDER_MAIL, // Sender email
+      to: toEmail, // Receiver email
+      subject: subject, // Email subject
+      html: htmlContent, // Email content in HTML format
+    };
+    try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully:", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email");
+  }
+};
+
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  console.log("inside:", email);
+  const subject = "Verify your email";
+  const htmlContent = VERIFICATION_EMAIL_TEMPLATE.replace(
+    "{verificationCode}",
+    verificationToken
+  );
+
+  try {
+    await sendEmail(email, subject, htmlContent);
+    //console.log("Verification email sent");
+  } catch (error) {
+    console.error("Error sending verification email", error);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const subject = "Welcome to Passman";
+  const htmlContent = WELCOME_EMAIL_TEMPLATE.replace("{name}", name);
+
+  try {
+    await sendEmail(email, subject, htmlContent);
+    console.log("Welcome email sent");
+  } catch (error) {
+    console.error("Error sending welcome email", error);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const subject = "Reset your password";
+  const htmlContent = PASSWORD_RESET_REQUEST_TEMPLATE.replace(
+    "{resetURL}",
+    resetURL
+  );
+
+  try {
+    await sendEmail(email, subject, htmlContent);
+    console.log("Password reset email sent");
+  } catch (error) {
+    console.error("Error sending password reset email", error);
+  }
+};
+
+export const sendResetSuccessfulEmail = async (email) => {
+  const subject = "Password reset successful";
+  const htmlContent = PASSWORD_RESET_SUCCESS_TEMPLATE;
+
+  try {
+    await sendEmail(email, subject, htmlContent);
+    console.log("Password reset successful email sent");
+  } catch (error) {
+    console.error("Error sending password reset successful email", error);
+  }
+};
---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,102 @@
+
+import {create} from "zustand";
+import axios from "axios";
+import ForgotPasswordPage from "../pages/ForgotPasswordPage";
+
+const API_URI="http://localhost:4000/api/auth";
+
+axios.defaults.withCredentials=true
+
+export const useAuthStore = create((set) => ({
+    user: null,
+    isAuthenticating: false,
+    error: null,
+    isLoading:false,
+    isCheckingAuth:true,
+
+    signup:async(email,password,name)=>{
+        set({isLoading:true,error:null})
+        try{
+            const response=await axios.post(`${API_URI}/signup`,{email,password,name})
+            set({user:response.data.user,isAuthenticated:true,isLoading:false})
+        }
+        catch(error){
+            set({error:error.response.data.message || "Error signing up",isLoading:false})
+            throw error
+            
+            
+        }
+    },
+
+    verifyEmail: async (code) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URI}/verify-email`, { code });
+			set({ user: response.data.user, isAuthenticated: true, isLoading: false });
+			return response.data;
+		} catch (error) {
+			set({ error: error.response.data.message || "Error verifying email", isLoading: false });
+			throw error;
+		}
+	},
+
+    login:async(email,password)=>{
+        set({isLoading:true,error:null})
+        try{
+            const response=await axios.post(`${API_URI}/login`,{email,password})
+            set({user:response.data.user,error:null,isAuthenticated:true,isLoading:false})
+        }
+        catch(error){
+            set({error:error.response?.data?.message || "Error logging in",isLoading:false})
+            throw error
+        }
+    },
+
+    logout:async()=>{
+        set({ isLoading: true, error: null });
+		try {
+			await axios.post(`${API_URI}/logout`);
+			set({ user: null, isAuthenticated: false, error: null, isLoading: false });
+		} catch (error) {
+			set({ error: "Error logging out", isLoading: false });
+			throw error;
+		}
+    },
+
+    checkAuth:async()=>{
+        set({isCheckingAuth:true,error:null})
+        try{
+            const response=await axios.get(`${API_URI}/check-auth`)
+            set({user:response.data.user,isAuthenticated:true,isCheckingAuth:false})
+        }
+        catch(error){
+            set({error:null,isCheckingAuth:false,isAuthenticated:false})
+        }
+    },
+
+    forgotPassword:async(email)=>{
+        set({isLoading:true,error:null})
+        try{
+            const response=await axios.post(`${API_URI}/forgot-password`,{email})
+            set({isLoading:false,message:response.data.message})
+        }
+        catch(error){
+            set({error:error.response.data.message || "Error sending email",isLoading:false})
+            throw error
+        }
+    },
+    
+    resetPassword:async(password,token)=>{
+        set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URI}/reset-password/${token}`, { password });
+			set({ message: response.data.message, isLoading: false });
+		} catch (error) {
+			set({
+				isLoading: false,
+				error: error.response.data.message || "Error resetting password",
+			});
+			throw error;
+		}
+    },
+}));
